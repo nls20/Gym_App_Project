5ba4db819b2c1d74ab8f12b8b8d3a25251d0286b
nls20 <nls20@hotmail.co.uk>
2021-02-06 15:23:37 +0000

    Added acreate and delete new sessions

--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -15,3 +15,21 @@ def show(id):
     session = session_repository.select(id)
     members = session_repository.members(session)
     return render_template("sessions/show.html", session = session, members = members)
+
+@sessions_blueprint.route("/sessions/new", methods=['GET'])
+def new_session():
+    sessions = session_repository.select_all()
+    return render_template("sessions/new.html", sessions = sessions)
+
+@sessions_blueprint.route("/sessions", methods = ['POST'])
+def create_session():
+    name = request.form['name']
+    category = request.form['category']
+    session = Session(name, category)
+    session_repository.save(session)
+    return redirect("/sessions")
+
+@sessions_blueprint.route("/sessions/<id>/delete", methods=["POST"])
+def delete_session(id):
+    session_repository.delete(id)
+    return redirect('/sessions')
--- repositories/session_repository.py
+++ repositories/session_repository.py
@@ -46,3 +46,8 @@ def members(session):
         members.append(member)
 
     return members
+
+def delete(id):
+    sql = "DELETE FROM sessions WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
